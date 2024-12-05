26ecb617d4f96f1e173ccc4eb5cce6c1d9ecbbc1
turly221 <feifan@scntist.com>
2024-12-05 14:16:53 +0000

    commit patch 25389217

--- src/gd_webp.c
+++ src/gd_webp.c
@@ -142,6 +142,14 @@ void gdImageWebpCtx (gdImagePtr im, gdIOCtx * outfile, int quantization)
 		return;
 	}
 
+	if (overflow2(gdImageSX(im), 4)) {
+		return;
+	}
+
+	if (overflow2(gdImageSX(im) * 4, gdImageSY(im))) {
+		return;
+	}
+
 	argb = (uint8_t *)gdMalloc(gdImageSX(im) * 4 * gdImageSY(im));
 	if (!argb) {
 		return;
